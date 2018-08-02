c139bf1766fc5651154a98d1d306b50fc0d109f4
Ramon Bartl <rb@ridingbytes.com>
2018-08-02 10:58:57 +0200

    Headers updated

--- src/senaite/databox/subscribers/on_object_added.py
+++ src/senaite/databox/subscribers/on_object_added.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.DATABOX
+#
+# Copyright 2018 by it's authors.
 
 from bika.lims.idserver import renameAfterCreation
 from senaite.databox import logger
