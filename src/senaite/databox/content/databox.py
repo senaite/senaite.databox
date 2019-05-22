a5ed38692b1767a825d38e872ae33658618134f2
Ramon Bartl <rb@ridingbytes.com>
2019-05-22 13:23:57 +0200

    Use IBasic behavior for DataBoxes

--- src/senaite/databox/content/databox.py
+++ src/senaite/databox/content/databox.py
@@ -6,20 +6,12 @@
 
 from plone.dexterity.content import Item
 from plone.supermodel import model
-from senaite.databox import senaiteMessageFactory as _
-from zope import schema
 from zope.interface import implements
 
 
 class IDataBox(model.Schema):
     """The DataBox container
     """
-
-    title = schema.TextLine(
-        title=_(u"Title"),
-        description=_(u"Please name your DataBox"),
-        required=True,
-    )
 
 
 class DataBox(Item):
