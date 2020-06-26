b0c7f537d37e5f8960758f0e1482e14f7b7e9b48
Ramon Bartl <rb@ridingbytes.com>
2020-06-26 09:15:39 +0200

    Link to parent for Analyses

--- src/senaite/databox/converters.py
+++ src/senaite/databox/converters.py
@@ -5,11 +5,16 @@ import six
 from bika.lims import api
 from bika.lims.utils import get_link
 from DateTime import DateTime
+from plone.protect.utils import addTokenToUrl
 from Products.ATContentTypes.utils import DT2dt
+
+LINK_TO_PARENT_TYPES = [
+    "Analysis",
+]
 
 
 def to_string(obj, key, value, **kw):
-    """Convert to UTF8
+    """to string
     """
     if isinstance(value, six.string_types):
         value = api.safe_unicode(value).encode("utf-8")
@@ -19,16 +24,19 @@ def to_string(obj, key, value, **kw):
 
 
 def to_link(obj, key, value, **kw):
-    """Link to object
+    """to link
     """
     value = to_string(obj, key, value)
     if not value:
         return ""
-    return get_link(api.get_url(obj), value)
+    if api.get_portal_type(obj) in LINK_TO_PARENT_TYPES:
+        obj = api.get_parent(obj)
+    url = addTokenToUrl(api.get_url(obj))
+    return get_link(url, value)
 
 
 def to_date(obj, key, value, dfmt="%d.%m.%Y"):
-    """Format value to date
+    """to date
     """
     if not isinstance(value, DateTime):
         return ""
@@ -36,6 +44,6 @@ def to_date(obj, key, value, dfmt="%d.%m.%Y"):
 
 
 def to_long_date(obj, key, value, dfmt="%d.%m.%Y %H:%M"):
-    """Format value to long date
+    """to long date
     """
     return to_date(obj, key, value, dfmt=dfmt)
